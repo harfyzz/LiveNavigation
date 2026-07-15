f606042b058d8971bb47308b1975ebdb9e0f3794
Afeez <harfyzz@gmail.com>
2026-07-15 12:42:49 +0100

    Initial Commit

---
+++ LiveNavigation/LiveNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveNavigationApp.swift
+//  LiveNavigation
+//
+//  Created by Afeez Yunus on 15/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct LiveNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
